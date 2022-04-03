8ba5e75f2a3a28e10b2498480e29e9c04d679de0
TomasHurtado <tomas221298@gmail.com>
2022-04-03 20:18:56 -0300

    agrego apis

--- src/components/GenresInDb.js
+++ src/components/GenresInDb.js
@@ -1,71 +1,49 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 
 function GenresInDb() {
+  const [categories,setCategory] = useState([]);
+  useEffect(()=>{
+      fetch("api/productos")
+      .then(response=>response.json()
+      ).then(data => {
+              setCategory(data.countByCategory)
+              
+          
+      })
+      .catch(error => console.log(error))
+  },[])
+
+
   return (
     <div className="col-lg-6 mb-4">
       <div className="card shadow mb-4">
         <div className="card-header py-3">
           <h5 className="m-0 font-weight-bold text-gray-800">
-            Categorias en Base de datos
+            Productos por Categoría
           </h5>
         </div>
         <div className="card-body">
           <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
+            {
+              categories.map((category,i)=>{
+                return(
+                  <div className="col-lg-6 mb-4" key={i}>
+                  <div className="card bg-dark text-white shadow">
+                    <div className="card-body">{category.name} {category.amount}</div>
+                  </div>
+                </div>
+                )
+                
+              })
+              
+            }
+          
+            
           </div>
         </div>
       </div>
     </div>
   );
-}
 
-export default GenresInDb;
+}
