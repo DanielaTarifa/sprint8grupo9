aeaa9611cf0bd163612eec2002958a92a707b2f1
FlorenciaCanteros <flor.nicole16@gmail.com>
2022-04-03 23:24:25 -0300

    agrego api

--- src/components/LastMovieInDb.js
+++ src/components/LastMovieInDb.js
@@ -10,8 +10,8 @@ function LastMovieInDb(){
             response.json()
         ).then(data => {
             
-                setProduct(data)
-                console.log(data)
+                setProduct(data.ultimoproduct)
+                console.log(data.ultimoproduct)
                 
             
         })
@@ -23,13 +23,14 @@ function LastMovieInDb(){
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Último producto cargado</h5>
+                    <h5 className="m-0 font-weight-bold text-gray-800">Último producto cargado:<br></br> {product.name}</h5>
                 </div>
                 <div className="card-body">
+    
                     <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}}  alt="producto"/>
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} alt="producto"/>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
+                    <p>{product.description}</p>
                     <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle</a>
                 </div>
             </div>
